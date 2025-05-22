8c3c36a84fca52f994ac1a01a98a2a2d6a83ad1b
Alexandra Bunch <121991056+oldfashionedjelly@users.noreply.github.com>
2025-05-22 16:04:31 -0400

    Create new.ino

---
+++ new.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+const int servoPin = 9;
+
+Servo myServo;
+
+float currentAngle = 0;
+bool direction = true;
+unsigned long lastMoveTime = 0;
+const unsigned long moveInterval = 100;  // adjust for speed
+
+void setup() {
+  myServo.attach(servoPin);
+  myServo.write(currentAngle);
+}
+
+void loop() {
+  
+  if (millis() - lastMoveTime >= moveInterval) {
+    currentAngle += direction ? 1 : -1;
+    if (currentAngle >= 180) direction = false;
+    if (currentAngle <= 0) direction = true;
+
+    myServo.write(currentAngle);
+    lastMoveTime = millis();
+  }
+}
